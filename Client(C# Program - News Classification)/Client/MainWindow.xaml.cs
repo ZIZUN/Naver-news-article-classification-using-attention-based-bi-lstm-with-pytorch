0256518770bca3c88bb63b4e22000a6891fe97a3
ZIZUN <cap1232@naver.com>
2019-12-31 22:12:26 +0900

    news classification
    전북대학교 인공지능 프로젝트
    멘토 - 이영훈

---
+++ Client(C# Program - News Classification)/Client/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ZeroMQ;
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            using (var requester = new ZSocket(ZSocketType.REQ))
+            {
+                requester.Connect("tcp://127.0.0.1:5858"); // Connect
+                string requestText = text.Text; 
+
+                requester.Send(new ZFrame(requestText)); // Send
+
+                using (ZFrame reply = requester.ReceiveFrame()) // Receive
+                {
+                    label.Content = reply.ReadString();
+                }
+            }
+        }
+    }
+}
